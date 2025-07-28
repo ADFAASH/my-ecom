7057b4f7cd41b7a0916436b2c4eeacef76724589
azdine ben <azdinevlogs1@gmail.com>
2025-07-28 22:35:16 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -111,7 +111,7 @@ export default function Home() {
           className="absolute top-0 left-0 w-full h-full object-cover"
         >
           <source src="https://res.cloudinary.com/dsddrhaxq/video/upload/v1753485329/hero_aoydj7.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
+          Your browser does not support the video tag
         </video>
 
         <div className="absolute inset-0 bg-black/40 z-0"></div>
